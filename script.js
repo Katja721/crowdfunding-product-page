de964cda48fe2503fe2de420d553e27452245c96
Katja721 <katja.debevc72@gmail.com>
2022-06-16 21:00:01 +0200

    add second modal

--- script.js
+++ script.js
@@ -12,6 +12,15 @@ const { x, y } = firstModal.getBoundingClientRect();
 const siteHeight = `${body.clientHeight}px`;
 const siteWidth = `${body.clientWidth}px`;
 const continueElements = document.querySelectorAll(".continue");
+const complitedModal = document.querySelector(".complitedModal");
+const complitedButton = document.querySelector(".complited-button");
+const bookmark = document.querySelector(".bookmark");
+const progress = document.querySelector(".progress");
+const collectedElement = document.querySelector(".collected");
+const backers = document.querySelector(".numOfBackers");
+let numBackers = 5007;
+let collected = 89914;
+const needed = 100000;
 overlay.style.height = siteHeight;
 overlay.style.width = siteWidth;
 
@@ -22,13 +31,40 @@ function selectPledge(pledge) {
   pledge.querySelector(".fill").style.display = "block";
   pledge.style.border = "1px solid var(--dark-cyan)";
 }
+function progressMeter(collected, need = needed) {
+  let percentage = (collected / need) * 100;
+  progress.style.width = `${percentage}%`;
+}
 continueElements.forEach((el) =>
-  el.addEventListener("click", function () {
+  el.addEventListener("click", function (e) {
+    const click = e.target;
+    const pledgeValue =
+      click.closest(".pledgeEl").querySelector(".number").value ||
+      click.closest(".pledgeEl").querySelector(".number").placeholder;
     firstModal.classList.add("hidden");
+    complitedModal.style.display = "flex";
+    collected += Number(pledgeValue);
+    if (collected > needed) collected = needed;
+    progressMeter(collected);
+    collectedElement.innerText = `$${collected}`;
+    numBackers++;
+    backers.innerText = `${numBackers}`;
   })
 );
 back.addEventListener("click", function () {
   overlay.classList.remove("hidden");
+});
+complitedButton.addEventListener("click", function () {
+  overlay.classList.add("hidden");
+});
+bookmark.addEventListener("click", function (e) {
+  const click = e.target;
+  bookmark.innerHTML = `
+  <svg width="56" height="56" xmlns="http://www.w3.org/2000/svg"class="bookmark-icon><g fill="none" fill-rule="evenodd"><circle fill="#147b74" cx="28" cy="28" r="28"/><path fill="#ffffff" d="M23 19v18l5-5.058L33 37V19z" "/></g></svg>
+  <p>Bookmarked</p>
+  `;
+  const bookmarkIcon = document.querySelector(".bookmark-icon");
+  bookmark.style.color = "var(--dark-cyan)";
 });
 selectReward.forEach((reward) =>
   reward.addEventListener("click", function () {
@@ -51,3 +87,5 @@ firstModal.addEventListener("click", function (e) {
 closeModal.addEventListener("click", function () {
   overlay.classList.add("hidden");
 });
+
+progressMeter(collected, needed);
