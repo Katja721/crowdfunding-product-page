e2ae6191f8e3fa6bc1ca239a58ed613e155f74c1
Katja721 <katja.debevc72@gmail.com>
2022-06-18 15:44:09 +0200

    refactoring code

--- script.js
+++ script.js
@@ -8,9 +8,6 @@ const overlay = document.querySelector(".overlay");
 const back = document.querySelector(".back");
 const selectReward = document.querySelectorAll(".reward");
 const body = document.querySelector("body");
-const { x, y } = firstModal.getBoundingClientRect();
-const siteHeight = `${body.clientHeight}px`;
-const siteWidth = `${body.clientWidth}px`;
 const continueElements = document.querySelectorAll(".continue");
 const complitedModal = document.querySelector(".complitedModal");
 const complitedButton = document.querySelector(".complited-button");
@@ -18,14 +15,15 @@ const bookmark = document.querySelector(".bookmark");
 const progress = document.querySelector(".progress");
 const collectedElement = document.querySelector(".collected");
 const backers = document.querySelector(".numOfBackers");
-let numBackers = 5007;
 let collected = 89914;
 const needed = 100000;
-overlay.style.height = siteHeight;
-overlay.style.width = siteWidth;
 
-console.log(siteHeight);
-console.log(x, y);
+function setOverlaySize() {
+  const siteHeight = `${body.clientHeight}px`;
+  const siteWidth = `${body.clientWidth}px`;
+  overlay.style.height = siteHeight;
+  overlay.style.width = siteWidth;
+}
 function selectPledge(pledge) {
   pledge.querySelector(".pledgeEl").classList.remove("hidden");
   pledge.querySelector(".fill").style.display = "block";
@@ -35,27 +33,41 @@ function progressMeter(collected, need = needed) {
   let percentage = (collected / need) * 100;
   progress.style.width = `${percentage}%`;
 }
+function removeOverlay() {
+  overlay.classList.add("hidden");
+}
+function addOverlay() {
+  overlay.classList.remove("hidden");
+}
+setOverlaySize();
 continueElements.forEach((el) =>
   el.addEventListener("click", function (e) {
     const click = e.target;
+    let numBackers = 5007;
     const pledgeValue =
       click.closest(".pledgeEl").querySelector(".number").value ||
       click.closest(".pledgeEl").querySelector(".number").placeholder;
+
+    //swirch from first to second modal
     firstModal.classList.add("hidden");
     complitedModal.style.display = "flex";
+
+    //modify money collected value
     collected += Number(pledgeValue);
     if (collected > needed) collected = needed;
     progressMeter(collected);
-    collectedElement.innerText = `$${collected}`;
+    collectedElement.innerText = `${collected}`;
+
+    //modify number of backers
     numBackers++;
     backers.innerText = `${numBackers}`;
   })
 );
 back.addEventListener("click", function () {
-  overlay.classList.remove("hidden");
+  addOverlay();
 });
 complitedButton.addEventListener("click", function () {
-  overlay.classList.add("hidden");
+  removeOverlay();
 });
 bookmark.addEventListener("click", function (e) {
   const click = e.target;
@@ -63,18 +75,17 @@ bookmark.addEventListener("click", function (e) {
   <svg width="56" height="56" xmlns="http://www.w3.org/2000/svg"class="bookmark-icon><g fill="none" fill-rule="evenodd"><circle fill="#147b74" cx="28" cy="28" r="28"/><path fill="#ffffff" d="M23 19v18l5-5.058L33 37V19z" "/></g></svg>
   <p>Bookmarked</p>
   `;
-  const bookmarkIcon = document.querySelector(".bookmark-icon");
   bookmark.style.color = "var(--dark-cyan)";
 });
 selectReward.forEach((reward) =>
   reward.addEventListener("click", function () {
+    const { x, y } = firstModal.getBoundingClientRect();
     window.scroll(x, y);
-    overlay.classList.remove("hidden");
+    addOverlay();
   })
 );
 firstModal.addEventListener("click", function (e) {
   const cliked = e.target;
-  console.log(cliked);
   if (cliked.closest(".modalEl").classList.contains("noStock")) return;
   const chosenElement = cliked.closest(".modalEl");
   fillElemnts.forEach((fill) => (fill.style.display = "none"));
@@ -85,7 +96,7 @@ firstModal.addEventListener("click", function (e) {
   selectPledge(chosenElement);
 });
 closeModal.addEventListener("click", function () {
-  overlay.classList.add("hidden");
+  removeOverlay();
 });
 
 progressMeter(collected, needed);
